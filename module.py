98c51f6f32e7637fbaed807b5018584c7bb3a6aa
marcinh <marcin.hydzik@gmail.com>
2023-01-24 01:04:15 +0100

    dodanie drugiej funkcji

--- module.py
+++ module.py
@@ -1,21 +1,42 @@
 #funkcja wczytująca dataset
 
-def data_imp(file_path = input('data set path')):  #, etykiety = input('contain headers? (Y/N)')):
+def data_imp(file_path = input('type in dataset path'), etykiety = input('does it contain headers? (Y/N)')):
     
 
     f = open(file_path, 'r')
 
     data_list = []
-    # header_list = []
+    header_list = []
+    counter = 0
 
     for linia in f:
 
         linia_edited = linia.replace('\n', '')
-
         linia_edited2 = linia_edited.split(',')
 
-        data_list.append(linia_edited2)
-        # data_list.append(linia2)
+        if etykiety == 'Y' and counter == 0:
 
-    print(data_list)
-    # print(header_list)
+            header_list = [i for i in linia_edited2] 
+            counter += 1
+            
+        else:
+            
+            data_list.append(linia_edited2)
+
+    return header_list
+    
+
+def header_check():
+
+    header_list = data_imp()
+    
+    if bool(header_list) == False:
+
+        print('zbiór nie posiada etykiet')
+    
+    else:
+
+        print('data set zawiera następujące etykiety:')
+    
+        for i in header_list:
+            print(i)
